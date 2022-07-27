ad81186fb602d3a2ab4869b2c85629d2b810e31e
jpgJanhvi <janhvipandey4002@gmail.com>
2022-07-27 22:47:19 +0530

    functions

---
+++ functions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void printhello();
+void goodbye();
+
+int main()
+{
+    printhello();
+    printgoodbye();
+    return 0;
+}
+void printhello()
+{
+    printf("hello!\n");
+}
+void printgoodbye()
+{
+    printf("goodbye");
+}
