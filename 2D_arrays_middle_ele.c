70d356309196fe463c2c9edc13c5453a14b4b0cd
janhvi pandey <94743766+jpgJanhvi@users.noreply.github.com>
2022-07-29 18:08:35 +0530

    Add files via upload

---
+++ 2D_arrays_middle_ele.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],m,n,i,j,sum=0;
+    printf("enter the order of matrix:");
+    scanf("%d%d",&m,&n);
+    printf("enter the matrix :");
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("print the matrix is:");
+    if()
