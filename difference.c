490ba5d23f0d05b45e9f22f0c1ebc4a82c399606
janhvi pandey <94743766+jpgJanhvi@users.noreply.github.com>
2022-10-22 21:36:20 +0530

    Add files via upload

--- difference.c
+++ difference.c
@@ -1,72 +1,74 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a[10],b[10],c[10],d[10],m=0,k=0,n1,n2,l,i,j;
+    int a[10], b[10], c[10], d[10], m = 0, k = 0, n1, n2, l, i, j;
     printf("Enter size of set A: ");
-    scanf("%d",&n1);
+    scanf("%d", &n1);
     printf("Enter element of set: ");
-    for( i=0;i<n1;i++)
-        scanf("%d",&a[i]);
+    for (i = 0; i < n1; i++)
+        scanf("%d", &a[i]);
     printf("Enter size of set B: ");
-    scanf("%d",&n2);
+    scanf("%d", &n2);
     printf("Enter element of set: ");
-    for( i=0;i<n2;i++)
-        scanf("%d",&b[i]);
-   
-    
+    for (i = 0; i < n2; i++)
+        scanf("%d", &b[i]);
+
     // logic for find A-B
-    for( i=0;i<n1;i++)
+    for (i = 0; i < n1; i++)
     {
-        for(j=0;j<n2;j++)
+        for (j = 0; j < n2; j++)
         {
-            if(b[j]==a[i])
-             break;
+            if (b[j] == a[i])
+                break;
         }
-        if(j==n2)
+        if (j == n2)
         {
-            for(l=0;l<k;l++)
+            for (l = 0; l < k; l++)
             {
-                if(c[l]==a[i])
-                 break;
+                if (c[l] == a[i])
+                    break;
             }
-            if(l==k)
+            if (l == k)
             {
-                c[k]=a[i];
+                c[k] = a[i];
                 k++;
             }
-        }   
-    }
-    
-    // logic for find B-A
-    for( i=0;i<n2;i++)
-    {
-        for(j=0;j<n1;j++){
-            if(b[i]==a[j])
-             break;
         }
-        if(j==n1)
+    }
+
+    // logic for find B-A
+    for (i = 0; i < n2; i++)
+    {
+        for (j = 0; j < n1; j++)
         {
-            for(l=0;l<m;l++) {
-                if(d[l]==b[i])
-                 break;
+            if (b[i] == a[j])
+                break;
+        }
+        if (j == n1)
+        {
+            for (l = 0; l < m; l++)
+            {
+                if (d[l] == b[i])
+                    break;
             }
-            if(l==m){
-                d[m]=b[i];
+            if (l == m)
+            {
+                d[m] = b[i];
                 m++;
             }
-        }    
+        }
     }
     printf("Difference of A-B is:-\n");
-    for(i=0;i<k;i++)
+    for (i = 0; i < k; i++)
     {
-         printf("%d ",c[i]);
+        printf("%d ", c[i]);
     }
     printf("\n");
     printf("Difference of B-A is:-\n");
-    for(i=0;i<m;i++)
+    for (i = 0; i < m; i++)
     {
-         printf("%d ",d[i]);
+        printf("%d ", d[i]);
     }
-    printf("\nLAKSHYA DHAWAN");
-    return 0;   
+    printf("\nJANHVI PANDEY");
+    return 0;
 }
